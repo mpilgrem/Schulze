5a8bccce302b40d2cd13be546ec31e4ed47b2fa2
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-03-28 23:29:27 +0000

    Sort final result

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,9 @@ module Main
 import           Control.Exception ( throwIO )
 import           Control.Monad ( forM_ )
 import qualified Data.ByteString as BS
+import           Data.List ( sortOn )
 import qualified Data.Map.Strict as Map
+import           Data.Ord ( Down (..) )
 import           Data.Text ( unpack )
 import           Data.Vector.Unboxed ( Vector, (!) )
 import           Data.Vote.IO ( decodeEither )
@@ -63,40 +65,50 @@ printBeats :: Int -> WeightedEdges -> IO ()
 printBeats n we = do
   title "Beats"
   heading n
-  forM_ ([0 .. n - 1] :: [Int]) $ \i -> do
+  forM_ indicesN $ \i -> do
     putStr $ toCandRef i : ": "
-    forM_ ([0 .. n - 1] :: [Int]) $ \j -> do
+    forM_ indicesN $ \j -> do
       case Map.lookup (i, j) we of
         Nothing -> putStr "    "
         Just cost -> printf "%4d" cost
     putStr "\n"
+ where
+  indicesN = indices n
 
 -- | Helper function to output strongest beat path table.
 printStrongestPath :: Int -> Vector Int -> IO ()
 printStrongestPath n graph = do
   title "Strongest Path"
   heading n
-  forM_ ([0 .. n - 1] :: [Int]) $ \i -> do
+  forM_ indicesN $ \i -> do
     putStr $ toCandRef i : ": "
-    forM_ ([0 .. n - 1] :: [Int]) $ \j -> do
+    forM_ indicesN $ \j -> do
       printf "%4d" (graph ! mkIndex n (i, j))
     putStr "\n"
+ where
+  indicesN = indices n
 
 -- | Helper function to output ranking table.
 printRanking :: Int -> Vector Int -> IO ()
 printRanking n graph = do
   title "Ranking"
   heading n
-  forM_ ([0 .. n - 1] :: [Int]) $ \i -> do
+  forM_ (sortOn (Down . value) indicesN) $ \i -> do
     putStr $ toCandRef i : ": "
-    forM_ ([0 .. n - 1] :: [Int]) $ \j -> do
-      let cost = graph ! mkIndex n (i, j)
-          cost' = graph ! mkIndex n (j, i)
-      putStr $ case compare cost cost' of
-        LT -> "    "
-        EQ -> [' ', ' ', '=', toCandRef j]
-        GT -> [' ', ' ', '>', toCandRef j]
+    forM_ indicesN $ \j -> do
+      putStr $ snd $ count i j
     putStr "\n"
+ where
+  indicesN = indices n
+  value i = sum $ map (fst . (count i)) indicesN
+  count :: Int -> Int -> (Int, String)
+  count i j =
+    let cost = graph ! mkIndex n (i, j)
+        cost' = graph ! mkIndex n (j, i)
+    in  case compare cost cost' of
+          LT -> (0, "    ")
+          EQ -> (1, [' ', ' ', '=', toCandRef j])
+          GT -> (1, [' ', ' ', '>', toCandRef j])
 
 -- | Helper function to output a title.
 title :: String -> IO ()
@@ -109,8 +121,12 @@ title s = do
 heading :: Int -> IO ()
 heading n = do
   putStr "   "
-  forM_ ([0 .. n - 1] :: [Int]) $ \j -> printf "%4c" (toCandRef j)
+  forM_ (indices n) $ \j -> printf "%4c" (toCandRef j)
   putStr "\n"
   putStr "   "
   putStr $ concat $ replicate n " ---"
   putStr "\n"
+
+-- | Helper function to yield list of indices.
+indices :: Int -> [Int]
+indices n = [0 .. n - 1]
